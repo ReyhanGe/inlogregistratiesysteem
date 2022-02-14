c76de8fd903234c068fa59cf8f18fb89da49183f
ReyhanGe <334480@student.mboutrecht.nl>
2022-02-14 11:26:54 +0100

    ik heb 6.2 gemaakt.
     Maken van een registratieformulier - het formulier

--- registreer.php
+++ registreer.php
@@ -1,1 +1,22 @@
-registreer
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <form action="./index.php?content=register_script" method= "post">
+              <div class="mb-3">
+                <label for="inputEmail" class="form-label">Vul hier uw e-mailadres in:</label>
+                <input name="email" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">Uw privacy is gegarandeerd...</div>
+              </div>              
+             <!-- <button type="submit" class="btn btn-primary btn-lg btn-block" >Registreer</button> -->
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary btn-lg btn-block btn-outline-secondary" >Registreer</button>
+              </div>
+
+            </form>
+        </div>
+        <div class="col-12 col-sm-6">
+            <img src="img\animeshoe.jpeg" alt="shoe" class="w-75 mx-auto d-block ">
+        </div>
+    </div>
+    
+</div>
